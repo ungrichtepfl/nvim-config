98243bb2df4a35a68de2c655830749d6b492782d
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-08-31 21:57:56 +0200

    added toggleterm

--- lua/chrigi/plugins.lua
+++ lua/chrigi/plugins.lua
@@ -114,6 +114,9 @@ return packer.startup(function(use)
 		-- tag = 'release' -- To use the latest release
 	})
 
+	-- Toggleterm
+	use({ "akinsho/toggleterm.nvim", tag = "v2.*" })
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
