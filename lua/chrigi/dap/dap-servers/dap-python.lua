11b6c066864bba29bf6b99d38b1b3d09f03e6b5e
Christoph Ungricht <ch.ungricht@scewo.ch>
2022-09-07 09:07:31 +0200

    refactor

--- lua/chrigi/dap/dap-servers/dap-python.lua
+++ lua/chrigi/dap/dap-servers/dap-python.lua
@@ -6,17 +6,6 @@ end
 
 dap_python.setup("~/.virtualenvs/debugpy/bin/python") -- pip install depubpy
 
-local dap, dapui = require("dap"), require("dapui")
-dap.listeners.after.event_initialized["dapui_config"] = function()
-	dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-	dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-	dapui.close()
-end
-
 -- DAO Configurations
 table.insert(require("dap").configurations.python, {
 	type = "python",
