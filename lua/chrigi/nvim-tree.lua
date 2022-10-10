480623e318c2cda8cf266035eb7fb170a38117d8
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-10-10 22:18:34 +0200

    Fixed breaking changes after update

--- lua/chrigi/nvim-tree.lua
+++ lua/chrigi/nvim-tree.lua
@@ -15,7 +15,7 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 nvim_tree.setup({
 	disable_netrw = true,
 	hijack_netrw = true,
-	open_on_setup = false,
+	open_on_setup = true,
 	ignore_ft_on_setup = {
 		"startify",
 		"dashboard",
@@ -50,7 +50,6 @@ nvim_tree.setup({
 	},
 	view = {
 		width = 30,
-		height = 30,
 		hide_root_folder = false,
 		side = "left",
 		mappings = {
