fcb91102133c2fbef876a6322fd4a2bda4bbde9d
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-10-14 17:15:47 +0200

    cleaned up autopairs config

--- lua/chrigi/autopairs.lua
+++ lua/chrigi/autopairs.lua
@@ -14,13 +14,6 @@ npairs.setup({
 	},
 	disable_filetype = { "TelescopePrompt", "spectre_panel" }, -- to find filetype run `:echo &ft` when file is open
 	fast_wrap = {
-		map = "<M-e>",
-		chars = { "{", "[", "(", '"', "'" },
-		pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-		offset = 0, -- Offset from pattern match
-		end_key = "$",
-		keys = "qwertyuiopzxcvbnmasdfghjkl",
-		check_comma = true,
 		highlight = "PmenuSel",
 		highlight_grey = "LineNr",
 	},
