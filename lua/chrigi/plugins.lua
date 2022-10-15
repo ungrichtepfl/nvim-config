198da191a95a908f1c64af7de23050346b611905
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-10-15 17:59:01 +0200

    notify and neodev plugin installed

--- lua/chrigi/plugins.lua
+++ lua/chrigi/plugins.lua
@@ -44,10 +44,13 @@ packer.init({
 return packer.startup(function(use)
 	-- My plugins here
 	use("wbthomason/packer.nvim") -- Have packer manage itself
+
 	-- useful APIs
 	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
 	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 	use("kyazdani42/nvim-web-devicons")
+  -- neovim lua language server
+  use "folke/neodev.nvim"
 	-- better Bdelete
 	use("moll/vim-bbye")
 	-- make lua vim startup time faster
@@ -176,6 +179,8 @@ return packer.startup(function(use)
 		"kosayoda/nvim-lightbulb",
 		-- requires = "antoinemadec/FixCursorHold.nvim",
 	})
+  -- nicer notifications:
+  use("rcarriga/nvim-notify")
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
