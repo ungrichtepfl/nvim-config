1bae7b3733be0934178dbb8bfe3994e7e7954744
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-10-15 17:59:23 +0200

    updated deprecated command

--- lua/chrigi/lsp/handlers.lua
+++ lua/chrigi/lsp/handlers.lua
@@ -112,6 +112,6 @@ if not status_ok then
 	return M
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 return M
