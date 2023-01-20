057e8715971c37eda058b2ed801a356ddf360b26
Christoph Ungricht <christoph.ungricht@outlook.com>
2023-01-20 09:03:57 +0100

    feat: Added additional gitsigns shortcuts.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/chrigi/whichkey.lua
+++ lua/chrigi/whichkey.lua
@@ -104,21 +104,27 @@ local mappings = {
     j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
     k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
     l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+    L = { "<cmd>lua require 'gitsigns'.blame_line{full=true}<cr>", "Blame with changes" },
     p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
     r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
     R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
     s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+    S = { "<cmd>lua require 'gitsigns'.stage_buffer()<cr>", "Stage Buffer" },
     u = {
       "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
       "Undo Stage Hunk",
     },
+    d = {
+      "<cmd>lua require 'gitsigns'.diffthis()<cr>",
+      "Diff with HEAD",
+    },
+    D = {
+      "<cmd>lua require 'gitsigns'.toggle_deleted()<cr>",
+      "Toggle Deleted",
+    },
     o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
     b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
     c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-    d = {
-      "<cmd>Gitsigns diffthis HEAD<cr>",
-      "Diff",
-    },
     m = { "<cmd>GitConflictListQf<cr>", "List merge conflicts" },
   },
 
