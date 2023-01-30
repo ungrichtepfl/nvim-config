3b3bb97ba99a7da1d62b8271b80f7cc72c51266f
Christoph Ungricht <ch.ungricht@scewo.ch>
2023-01-30 09:27:24 +0100

    bugfix: Removed \ignore_ft_on_setup" in nvim-tree.
    Signed-off-by: Christoph Ungricht <ch.ungricht@scewo.ch>

--- lua/chrigi/nvim-tree.lua
+++ lua/chrigi/nvim-tree.lua
@@ -14,11 +14,6 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
   disable_netrw = true,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -78,3 +73,45 @@ nvim_tree.setup {
     ignore_list = {},
   },
 }
+
+local function open_nvim_tree(data)
+  local IGNORED_FT = {
+    "markdown",
+    "startify",
+    "alpha",
+    "dashboard",
+  }
+
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+
+  -- buffer is directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  if directory then
+    if not no_name and not directory then return end
+
+    -- change to the directory
+    if directory then vim.cmd.cd(data.file) end
+
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+  else
+    -- &ft
+    local filetype = vim.bo[data.buf].ft
+
+    -- only files please
+    if not real_file and not no_name then return end
+
+    -- skip ignored filetypes
+    if vim.tbl_contains(IGNORED_FT, filetype) then return end
+
+    -- open the tree but don't focus it
+    require("nvim-tree.api").tree.toggle { focus = false }
+  end
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
