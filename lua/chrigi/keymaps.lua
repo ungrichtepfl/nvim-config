463ae9d12f333cc95fdaa3912358aff00253372a
Christoph Ungricht <christoph.ungricht@outlook.com>
2023-06-03 15:25:24 +0200

    feat(keymaps): Added keymap to edit word when pressing enter.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/chrigi/keymaps.lua
+++ lua/chrigi/keymaps.lua
@@ -69,3 +69,6 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Change word with enter --
+keymap("n", "<CR>", "ciw", opts "Change word with enter")
