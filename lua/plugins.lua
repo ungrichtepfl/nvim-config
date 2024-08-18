871ffcc3b1d68a0825a18ea3900106074bdd92a1
Christoph Ungricht <ch.ungricht@scewo.ch>
2024-08-18 16:22:15 +0200

    feat: Added Gitgraph plugin.
    Signed-off-by: Christoph Ungricht <ch.ungricht@scewo.ch>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -417,6 +417,31 @@ lazy.setup {
     dependencies = "nvim-lua/plenary.nvim",
     config = function() require "config.diffview" end,
   },
+  {
+    "isakbm/gitgraph.nvim",
+    dependencies = { "sindrets/diffview.nvim" },
+    opts = {
+      hooks = {
+        -- Check diff of a commit
+        on_select_commit = function(commit)
+          vim.notify("DiffviewOpen " .. commit.hash .. "^!")
+          vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+        end,
+        -- Check diff from commit a -> commit b
+        on_select_range_commit = function(from, to)
+          vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+          vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader>gG",
+        function() require("gitgraph").draw({}, { all = true, max_count = 5000 }) end,
+        desc = "GitGraph - Draw",
+      },
+    },
+  },
 
   -- nice scrollbar
   {
