4ad784af8c0c82767c7006814c9601916ae6a49d
Christoph Ungricht <ch.ungricht@scewo.ch>
2024-08-19 08:36:09 +0200

    fix: Do not select first entry when non is selected in cmp.
    Signed-off-by: Christoph Ungricht <ch.ungricht@scewo.ch>

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -63,9 +63,17 @@ cmp.setup {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
-    -- Accept currently selected item. If none selected, `select` first item.
-    -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<CR>"] = cmp.mapping {
+      i = function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false }
+        else
+          fallback()
+        end
+      end,
+      s = cmp.mapping.confirm { select = true },
+      c = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
+    },
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
