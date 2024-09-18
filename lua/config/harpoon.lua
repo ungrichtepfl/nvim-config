106d065b6cef730875fe99944bab38fc170ba279
Christoph Ungricht <ch.ungricht@scewo.ch>
2024-09-18 09:34:07 +0200

    feat: Added additional keymaps to harpoon.
    Signed-off-by: Christoph Ungricht <ch.ungricht@scewo.ch>

--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -11,6 +11,12 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-x>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-d>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-s>", function() harpoon:list():next() end)
