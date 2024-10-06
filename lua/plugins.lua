91c8d14319c77ed616f07d7ea7b6cbe5c9b299dc
Christoph Ungricht <christoph.ungricht@outlook.com>
2024-10-06 11:20:05 +0200

    feat: Added better markdown preview plugin.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -401,8 +401,13 @@ lazy.setup {
 
   -- Markdown preview
   {
-    "iamcco/markdown-preview.nvim",
-    build = function() vim.fn["mkdp#util#install"]() end,
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
   },
 
   -- surround stuff
