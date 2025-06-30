9b40f66ae094b28a5c5ec6dffb24c7e9248e4762
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-01 00:19:49 +0700

    feat: Move lsp-config config to seperate file.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,32 +1,39 @@
 return {
-  "mason-org/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = {
-      "arduino_language_server",
-      "asm_lsp",
-      "gopls",
-      "jsonls",
-      "pyright",
-      "lua_ls",
-      "bashls",
-      "clangd", -- C/C++
-      "cmake",
-      "dockerls",
-      "marksman", -- Markdown
-      "yamlls",
-      "elmls",
-      "taplo", -- TOML
-      "lemminx", -- "XML"
-      "ts_ls",
-      "html",
-      "cssls",
-      "angularls",
-      "omnisharp", -- C#/dotnet
-      "zls",
-    },
-  },
-  dependencies = {
-    { "mason-org/mason.nvim", opts = {} },
+  {
     "neovim/nvim-lspconfig",
+    init = function() require "plugins.lsp.config" end,
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "arduino_language_server",
+        "asm_lsp",
+        "gopls",
+        "jsonls",
+        "pyright",
+        "lua_ls",
+        "bashls",
+        "clangd", -- C/C++
+        "cmake",
+        "dockerls",
+        "marksman", -- Markdown
+        "yamlls",
+        "elmls",
+        "taplo", -- TOML
+        "lemminx", -- "XML"
+        "ts_ls",
+        "html",
+        "cssls",
+        "angularls",
+        "omnisharp", -- C#/dotnet
+        "zls",
+      },
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
   },
 }
