3daf8ab11595e94534f650499d3d884b76010f2a
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-06-30 22:06:29 +0700

    feat: Add keymap for grep.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -4,5 +4,6 @@ return {
   opts = {},
   keys = {
     { "<leader>f", "<cmd> FZF<cr>", desc = "Find files" },
+    { "<leader>r", "<cmd> FzfLua live_grep<cr>", desc = "Grep word in all files" },
   },
 }
