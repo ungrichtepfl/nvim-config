77f568a7d8164b4be01784c17d71dadd3d5a063e
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-03 00:37:53 +0700

    feat: Add another oil git plugin.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -13,7 +13,7 @@ return {
         "mtime",
       },
       win_options = {
-        signcolumn = "yes:2",
+        signcolumn = "yes:2", -- NOTE: Only needed when refractalize/oil-git-status.nvim is used
       },
       git = {
         mv = function(_, _) return true end,
@@ -25,10 +25,49 @@ return {
     keys = { { "<leader>e", ":Oil<cr>", desc = "Toggle Oil" } },
   },
   {
+    "benomahony/oil-git.nvim",
+    dependencies = { "stevearc/oil.nvim" },
+    enabled = false, -- FIXME: Cursore is weirdly blinking
+    opts = {
+      highlights = {
+        OilGitModified = { fg = "#ff0000" }, -- Custom colors
+      },
+    },
+  },
+  {
     "refractalize/oil-git-status.nvim",
     dependencies = {
       "stevearc/oil.nvim",
     },
-    config = true,
+    enabled = true,
+    opts = {
+      show_ignored = true, -- show files that match gitignore with !!
+      symbols = {
+        index = {
+          ["!"] = "", -- Ignored (not tracked and in .gitignore)
+          ["?"] = "", -- Untracked
+          ["A"] = "", -- Added (A for added)
+          ["C"] = "", -- Copied
+          ["D"] = "", -- Deleted
+          ["M"] = "", -- Modified
+          ["R"] = "", -- Renamed
+          ["T"] = "", -- Type change (maybe symbolic link?)
+          ["U"] = "", -- Unmerged
+          [" "] = " ", -- Clean / nothing
+        },
+        working_tree = {
+          ["!"] = "", -- Ignored (not tracked and in .gitignore)
+          ["?"] = "", -- Untracked
+          ["A"] = "", -- Added in working tree
+          ["C"] = "", -- Copied
+          ["D"] = "", -- Deleted
+          ["M"] = "", -- Modified
+          ["R"] = "", -- Renamed
+          ["T"] = "", -- Type changed
+          ["U"] = "", -- Unmerged
+          [" "] = " ", -- Clean
+        },
+      },
+    },
   },
 }
