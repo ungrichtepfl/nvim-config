0a93e6f64c8109e3b107be74399887eb9d3392fa
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-07 14:06:51 +0700

    feat: Enable nice snacks plugins.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,7 @@ return {
   "folke/snacks.nvim",
   opts = {
     input = {},
+    bigfile = {}, -- Do not launch lsp and treesitter on bigfiles
+    quickfile = {}, -- Load file as quick as possible
   },
 }
