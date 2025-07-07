b78103a06f33171e9d1ab50e36e1b02069182435
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-07 12:49:24 +0700

    fix: Don't display notifications across the whole screen.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -4,6 +4,8 @@ return {
     level = vim.log.levels.DEBUG,
     stages = "fade",
     timeout = 2000,
+    max_width = function() return math.floor(vim.api.nvim_win_get_width(0) / 2) end,
+    render = "wrapped-compact",
   },
   enabled = function() return vim.env.COLORTERM == "truecolor" or vim.env.TERM == "xterm-truecolor" end,
   init = function()
