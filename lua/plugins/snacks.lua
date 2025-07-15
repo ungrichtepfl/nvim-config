511cd27dec19840afb877a2155d5daa9fbb139f9
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-15 18:38:28 +0800

    feat: Remove alpha use snacks.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,5 +4,12 @@ return {
     input = {}, -- Nicer input
     bigfile = {}, -- Do not launch lsp and treesitter on bigfiles
     quickfile = {}, -- Load file as quickly as possible
+    dashboard = {
+      sections = {
+        { section = "header"},
+        { section = "recent_files", align = "center", padding = 3},
+        { section = "startup"},
+      },
+    },
   },
 }
