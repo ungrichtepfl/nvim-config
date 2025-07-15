25e9a2216959a79262c8d3325e22ebe75335b905
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-15 19:56:24 +0800

    feat: Show recent files from current working directory.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,9 +6,14 @@ return {
     quickfile = {}, -- Load file as quickly as possible
     dashboard = {
       sections = {
-        { section = "header"},
-        { section = "recent_files", align = "center", padding = 3},
-        { section = "startup"},
+        { section = "header" },
+        {
+          section = "recent_files",
+          cwd = true,
+          align = "center",
+          padding = 3,
+        },
+        { section = "startup" },
       },
     },
   },
