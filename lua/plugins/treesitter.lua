151ff0e30447793bc7eb7d8c195b856f79360317
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-19 17:24:51 +0800

    feat: Update plugins.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -183,44 +183,17 @@ return {
         end
       end
 
-      -- HACK: Remove this code and uncomment the keymaps defined in "below"
-      local repeatable_move = require "nvim-treesitter-textobjects.repeatable_move"
+      local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
 
-      -- workaround for https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/775
-      -- thanks @seandewar
-      local function make_repeat_rhs(dir)
-        return function()
-          local keys = repeatable_move["repeat_last_move_" .. dir]()
-          if keys then vim.cmd(("normal! %d%s"):format(vim.v.count1, vim.keycode(keys))) end
-        end
-      end
-
-      vim.keymap.set({ "n", "x", "o" }, ";", make_repeat_rhs "next")
-      vim.keymap.set({ "n", "x", "o" }, ",", make_repeat_rhs "previous")
+      -- vim way: ; goes to the direction you were moving.
+      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
       -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-      vim.keymap.set({ "n", "x", "o" }, "f", repeatable_move.builtin_f_expr, { expr = true })
-      vim.keymap.set({ "n", "x", "o" }, "F", repeatable_move.builtin_F_expr, { expr = true })
-      vim.keymap.set({ "n", "x", "o" }, "t", repeatable_move.builtin_t_expr, { expr = true })
-      vim.keymap.set({ "n", "x", "o" }, "T", repeatable_move.builtin_T_expr, { expr = true })
-
-      -- -- FIXME: Uncomment as soon as the hack above is fixed (https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/775)
-      -- local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
-      --
-      -- -- Repeat movement with ; and ,
-      -- -- ensure ; goes forward and , goes backward regardless of the last direction
-      -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next, { expr = true })
-      -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous, { expr = true })
-      --
-      -- -- vim way: ; goes to the direction you were moving.
-      -- -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-      -- -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-      --
-      -- -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-      -- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
-      -- vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
-      -- vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
-      -- vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
     end,
   },
   {
