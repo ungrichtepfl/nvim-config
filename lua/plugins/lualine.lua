c0112052a3e7665d1122592ec03586d6d11f0dc2
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-20 12:36:10 +0800

    fix: Filetype and lsp.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -168,7 +168,7 @@ return {
       -- Lsp server name .
       function()
         local msg = "No Active Lsp"
-        local buf_ft = vim.api.nvim_get_option_value(0, "filetype")
+        local buf_ft = vim.api.nvim_get_option_value("filetype", { buf = 0 })
         local clients = vim.lsp.get_clients()
         if next(clients) == nil then return msg end
         for _, client in ipairs(clients) do
@@ -183,6 +183,12 @@ return {
     }
 
     -- Add components to right sections
+    ins_right {
+      function() return vim.api.nvim_get_option_value("filetype", { buf = 0 }) end,
+      cond = conditions.hide_in_width,
+      color = { fg = colors.green, gui = "bold" },
+    }
+
     ins_right {
       "o:encoding", -- option component same as &encoding in viml
       fmt = string.upper, -- I'm not sure why it's upper case either ;)
