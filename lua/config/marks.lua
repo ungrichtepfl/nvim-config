8ccaea234b7f29a69025e0f67f2df4a2c4a9dc52
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-21 21:55:36 +0800

    feat: Add persistent mark handling.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/config/marks.lua
+++ lua/config/marks.lua
@@ -2,27 +2,81 @@
 local utils = require "config.utils"
 
 local mark_state = {
+  version = 0,
   index = 1,
   marks = { "H", "J", "K", "L" },
   marks_set = {},
 }
 
-function mark_state:sync()
-  for _, m in ipairs(vim.fn.getmarklist()) do
-    for _, mark in ipairs(self.marks) do
-      if m.mark:sub(2, 2) == mark then table.insert(self.marks_set, mark) end
+local function get_dir() return vim.fn.stdpath "state" .. "/marks/" end
+
+local function get_file(dir)
+  local cwd = vim.fn.getcwd()
+  local name = utils.escape_path(cwd)
+  return dir .. name .. ".json"
+end
+
+function mark_state:load()
+  local filepath = get_file(get_dir())
+  if vim.fn.filereadable(filepath) == 0 then
+    -- File does not exist
+    return
+  end
+  local content = table.concat(vim.fn.readfile(filepath), "\n")
+  local ok, state = pcall(vim.json.decode, content)
+  if not ok then
+    -- File corrupt ignore
+    return
+  end
+
+  if
+    not (
+      type(state.version) == "number"
+      and type(state.index) == "number"
+      and type(state.marks_set) == "table"
+      and type(state.marks) == "table"
+      and state.index <= #self.marks
+    )
+  then
+    -- Wrong format: ignore
+    return
+  end
+
+  if state.version ~= self.version then
+    -- old version: ignore
+    return
+  end
+
+  self.index = state.index
+  self.marks_set = state.marks_set
+  for _, m in ipairs(self.marks) do
+    local fn = self.marks_set[m]
+    if fn then
+      local bufnr = vim.fn.bufadd(vim.fn.expand(fn))
+      vim.fn.bufload(bufnr)
+      vim.api.nvim_buf_set_mark(bufnr, m, 1, 0, {})
+      vim.api.nvim_buf_delete(bufnr, { unload = true })
+    else
+      vim.cmd("delmark " .. m)
     end
   end
+end
+
+function mark_state:save()
+  local filedir = get_dir()
+  if vim.fn.isdirectory(filedir) == 0 then vim.fn.mkdir(filedir, "p") end
+  local to_save = {}
+  for k, v in pairs(self) do
+    if type(v) == "table" or type(v) == "string" or type(v) == "number" or type(v) == "boolean" then to_save[k] = v end
+  end
+  local serialized = vim.json.encode(to_save)
+  local filepath = get_file(filedir)
+  vim.fn.writefile({ serialized }, filepath)
 end
 
 function mark_state:increase_index()
   self.index = self.index + 1
   if self.index > #self.marks then self.index = 1 end
-end
-
-function mark_state:decrease_index()
-  self.index = self.index - 1
-  if self.index < 1 then self.index = 1 end -- Should not happen
 end
 
 local function goto_mark(mark)
@@ -36,28 +90,31 @@ local function goto_mark(mark)
 end
 
 function mark_state:add_mark()
-  local mark = self.marks[self.index]
+  local mark = nil
+  for _, m in ipairs(self.marks) do
+    if self.marks_set[m] == nil then
+      mark = m
+      break
+    end
+  end
+  if mark == nil then
+    mark = self.marks[self.index]
+    self:increase_index()
+  end
+
   vim.cmd.normal("m" .. mark)
-  if #self.marks_set < #self.marks then table.insert(self.marks_set, mark) end
-  self:increase_index()
+  self.marks_set[mark] = utils.mark_filename(mark)
   vim.notify("Added mark: " .. mark)
 end
 
 function mark_state:remove_mark(mark)
-  local idx = -1
-  for i, m in ipairs(self.marks_set) do
-    if m == mark then
-      idx = i
-      break
-    end
-  end
-  if idx == -1 then
+  if self.marks_set[mark] == nil then
     vim.notify("Mark " .. mark .. " was not set", vim.log.levels.WARN)
     return
   end
   vim.cmd("delmark " .. mark)
-  table.remove(self.marks_set, idx)
-  self:decrease_index()
+  self.marks_set[mark] = nil
+  self.index = 1 -- Reset as there is space now
   vim.notify("Removed mark " .. mark)
 end
 
@@ -78,11 +135,11 @@ function mark_state:show_marks_fzf(fzf, items)
         if mark then self:remove_mark(mark) end
       end,
       ["ctrl-a"] = function(_)
-        local marks = {}
-        for _, m in ipairs(self.marks_set) do -- Make shallow copy
-          table.insert(marks, m)
+        local marks_set = {}
+        for m, fn in pairs(self.marks_set) do -- Make shallow copy
+          marks_set[m] = fn
         end
-        for _, m in ipairs(marks) do
+        for m, _ in pairs(marks_set) do
           self:remove_mark(m)
         end
       end,
@@ -103,8 +160,7 @@ end
 
 function mark_state:show_marks()
   local items = {}
-  for _, m in ipairs(self.marks_set) do
-    local fn = utils.mark_filename(m)
+  for m, fn in pairs(self.marks_set) do
     fn = vim.fn.fnamemodify(fn, ":.")
     if fn then table.insert(items, string.format("%s: %s", m, fn)) end
   end
@@ -130,5 +186,10 @@ keymap("n", "<leader><leader>m", "`M", { desc = "Go to global mark M" })
 local marks_group = vim.api.nvim_create_augroup("_marks", { clear = true })
 vim.api.nvim_create_autocmd("VimEnter", {
   group = marks_group,
-  callback = function() mark_state:sync() end,
+  callback = function() mark_state:load() end,
+})
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  group = marks_group,
+  callback = function() mark_state:save() end,
 })
--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -50,4 +50,18 @@ function M.goto_last_edited()
   if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
 end
 
+function M.goto_last_cursor_position()
+  local mark = vim.api.nvim_buf_get_mark(0, '"')
+  local lcount = vim.api.nvim_buf_line_count(0)
+  if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
+end
+
+function M.escape_path(path)
+  return path:gsub("([^%w%.%-])", function(c) return string.format("%%%02X", string.byte(c)) end)
+end
+
+function M.unescape_path(escaped)
+  return escaped:gsub("%%(%x%x)", function(hex) return string.char(tonumber(hex, 16)) end)
+end
+
 return M
