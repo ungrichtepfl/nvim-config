5d4e9dd7fd0e3b65d3b9660d04f511409ea153d3
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-23 22:09:56 +0800

    feat: Added neogit.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

---
+++ lua/plugins/git.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      -- Only one of these is needed.
+      "ibhagwan/fzf-lua", -- optional
+    },
+    keys = {
+      {
+        "<leader>vs",
+        "<cmd>Neogit<cr>",
+        desc = "Show git status",
+      },
+      {
+        "<leader>vc",
+        "<cmd>Neogit commit<cr>",
+        desc = "Git commit",
+      },
+      {
+        "<leader>vpl",
+        "<cmd>Neogit pull<cr>",
+        desc = "Git pull",
+      },
+      {
+        "<leader>vpu",
+        "<cmd>Neogit push<cr>",
+        desc = "Git pull",
+      },
+      {
+        "<leader>vl",
+        "<cmd>Neogit log<cr>",
+        desc = "Git log",
+      },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "DiffviewOpen",
+    keys = {
+      {
+        "<leader>vq",
+        "<cmd>DiffviewClose<cr>",
+        desc = "Close Diffview",
+      },
+      {
+        "<leader>vo",
+        "<cmd>DiffviewOpen<cr>", -- NOTE: Use :tabclose to close (see keymaps)
+        desc = "Open Diffview",
+      },
+    },
+  },
+}
