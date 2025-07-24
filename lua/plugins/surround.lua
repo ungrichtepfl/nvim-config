90bf6f02f44905706a193236aab8d407e4e26d55
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-24 20:32:40 +0800

    feat: Easier remap for surround plugin.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -2,4 +2,12 @@ return {
   "kylechui/nvim-surround",
   event = "VeryLazy",
   opts = {},
+  keys = {
+    {
+      "<C-g><C-s>", -- Easier keymap
+      "<C-g>S",
+      mode = "i",
+      remap = true,
+    },
+  },
 }
