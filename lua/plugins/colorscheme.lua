9cd71fd45c9d0d3acd90fe8338732770a78ff14d
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-30 20:08:42 +0800

    feat: Played around with colorthemes.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,56 @@
+local color = "kanso-zen"
+-- local color = "tokyonight-moon"
+-- local color = "kanagawa-dragon"
+-- local color = "iceberg-dark"
+
 return {
-  "webhooked/kanso.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    compile = true,
+  {
+    "oahlen/iceberg.nvim",
+    lazy = false,
+    enabled = color:find "^iceberg" ~= nil,
+    priority = 1000,
+    init = function()
+      -- load the colorscheme here
+      vim.cmd.colorscheme(color)
+    end,
   },
-  build = ":KansoCompile",
-  init = function()
-    -- load the colorscheme here
-    vim.cmd [[colorscheme kanso-zen]]
-  end,
+  {
+    "webhooked/kanso.nvim",
+    lazy = false,
+    enabled = color:find "^kanso" ~= nil,
+    priority = 1000,
+    opts = {
+      compile = true,
+    },
+    build = ":KansoCompile",
+    init = function()
+      -- load the colorscheme here
+      vim.cmd.colorscheme(color)
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = color:find "^tokyonight" ~= nil,
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    init = function()
+      -- load the colorscheme here
+      vim.cmd.colorscheme(color)
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    enabled = color:find "^kanagawa" ~= nil,
+    priority = 1000,
+    opts = {
+      compile = true,
+    },
+    build = ":KanagawaCompile",
+    init = function()
+      -- load the colorscheme here
+      vim.cmd.colorscheme(color)
+    end,
+  },
 }
