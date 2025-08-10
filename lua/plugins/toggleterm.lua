d532cb6d507bb7a990a75427f067a86a51f37ddf
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-08-10 11:35:50 +0800

    feat: Add better keymaps for toggleterm.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,12 +5,41 @@ return {
   keys = {
     {
       "<c-t>",
-      "<cmd>ToggleTerm<cr>",
-      mode = { "n", "t", "i" },
       desc = "Open toggle term terminal",
+    },
+    {
+      "<leader>x",
+      '<cmd>TermExec cmd="%:p"<cr>',
+      mode = { "n" },
+      desc = "Run current file",
+    },
+    {
+      "<leader>tt",
+      "<cmd>ToggleTerm direction=tab<cr>",
+      mode = { "n", "t" },
+      desc = "Open toggle term terminal tab",
+    },
+    {
+      "<leader>tf",
+      "<cmd>ToggleTerm direction=float<cr>",
+      mode = { "n", "t" },
+      desc = "Open toggle term terminal floating",
+    },
+    {
+      "<leader>th",
+      "<cmd>ToggleTerm direction=horizontal<cr>",
+      mode = { "n", "t" },
+      desc = "Open toggle term terminal horizontally",
+    },
+    {
+      "<leader>tv",
+      "<cmd>ToggleTerm direction=vertical<cr>",
+      mode = { "n", "t" },
+      desc = "Open toggle term terminal vertically",
     },
   },
   opts = {
     shade_terminals = false, -- Do not make them look differently
+    open_mapping = [[<c-t>]],
   },
 }
