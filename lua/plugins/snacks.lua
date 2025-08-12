fd05dde2ac582ae03e263a7e6e97c96cc1d43987
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-08-12 13:45:53 +0800

    feat: Add quit options to startup dashboard.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,11 +5,20 @@ return {
     bigfile = {}, -- Do not launch lsp and treesitter on bigfiles
     quickfile = {}, -- Load file as quickly as possible
     dashboard = {
+      preset = {
+        keys = {
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
       sections = {
         { section = "header" },
         {
           section = "recent_files",
           cwd = true,
+          align = "center",
+        },
+        {
+          section = "keys",
           align = "center",
           padding = 3,
         },
