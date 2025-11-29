4ecedf2a060f10a019525245a942bc1b15e65235
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-11-29 15:31:57 +0800

    feat: Update plugins.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,56 +1,69 @@
-local color = "kanso-zen"
--- local color = "tokyonight-moon"
--- local color = "kanagawa-dragon"
--- local color = "iceberg-dark"
+local color = ""
+-- color = "ash"
+-- color = "iceberg-dark"
+color = "zen"
+-- color ="kanso-zen"
+-- color ="tokyonight-moon"
+-- color ="kanagawa-dragon"
+-- color = "mellifluous"
 
 return {
   {
+    "ramojus/mellifluous.nvim",
+    lazy = false,
+    enabled = color:find "^mellifluous$" ~= nil,
+    priority = 1000,
+    init = function() vim.cmd.colorscheme(color) end,
+  },
+  {
+    "bjarneo/ash.nvim",
+    lazy = false,
+    enabled = color:find "^ash$" ~= nil,
+    priority = 1000,
+    init = function() vim.cmd.colorscheme(color) end,
+  },
+  {
+    "nendix/zen.nvim",
+    lazy = false,
+    enabled = color:find "^zen$" ~= nil,
+    priority = 1000,
+    init = function() vim.cmd.colorscheme(color) end,
+  },
+  {
     "oahlen/iceberg.nvim",
     lazy = false,
-    enabled = color:find "^iceberg" ~= nil,
+    enabled = color:find "^iceberg$" ~= nil,
     priority = 1000,
-    init = function()
-      -- load the colorscheme here
-      vim.cmd.colorscheme(color)
-    end,
+    init = function() vim.cmd.colorscheme(color) end,
   },
   {
     "webhooked/kanso.nvim",
     lazy = false,
-    enabled = color:find "^kanso" ~= nil,
+    enabled = color:find "^kanso$" ~= nil,
     priority = 1000,
     opts = {
       compile = true,
     },
     build = ":KansoCompile",
-    init = function()
-      -- load the colorscheme here
-      vim.cmd.colorscheme(color)
-    end,
+    init = function() vim.cmd.colorscheme(color) end,
   },
   {
     "folke/tokyonight.nvim",
-    enabled = color:find "^tokyonight" ~= nil,
+    enabled = color:find "^tokyonight$" ~= nil,
     lazy = false,
     priority = 1000,
     opts = {},
-    init = function()
-      -- load the colorscheme here
-      vim.cmd.colorscheme(color)
-    end,
+    init = function() vim.cmd.colorscheme(color) end,
   },
   {
     "rebelot/kanagawa.nvim",
     lazy = false,
-    enabled = color:find "^kanagawa" ~= nil,
+    enabled = color:find "^kanagawa$" ~= nil,
     priority = 1000,
     opts = {
       compile = true,
     },
     build = ":KanagawaCompile",
-    init = function()
-      -- load the colorscheme here
-      vim.cmd.colorscheme(color)
-    end,
+    init = function() vim.cmd.colorscheme(color) end,
   },
 }
