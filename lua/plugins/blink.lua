ec25df326b65ac2c07d62245a512850c8f77cb3d
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-01-27 17:31:07 +0100

    fix: Load blink.cmp for command line.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,6 @@
 return {
   "saghen/blink.cmp",
-  event = "InsertEnter",
+  event = { "InsertEnter", "CmdlineEnter" },
   -- optional: provides snippets for the snippet source
   dependencies = { { "rafamadriz/friendly-snippets" }, { "echasnovski/mini.icons" }, { "Kaiser-Yang/blink-cmp-git" } },
 
