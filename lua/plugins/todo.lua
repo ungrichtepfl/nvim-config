9edbac9a48e9165e9a97f6d0fff81a39bdacba91
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-01-27 08:39:12 +0100

    feat: More loose in todo comments.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -10,21 +10,61 @@ return {
       FIX = {
         icon = " ", -- icon used for the sign, and in search results
         color = "error", -- can be a hex color, or a named color (see below)
-        alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "fix", "fixme", "bug", "fixit", "issue" }, -- a set of other keywords that all map to this FIX keywords
+        alt = {
+          "FIXME",
+          "BUG",
+          "FIXIT",
+          "ISSUE",
+          "fix",
+          "fixme",
+          "bug",
+          "fixit",
+          "issue",
+          "Fix",
+          "Fixme",
+          "Bug",
+          "Fixit",
+          "Issue",
+        }, -- a set of other keywords that all map to this FIX keywords
       },
-      TODO = { icon = " ", color = "info", alt = { "todo" } },
-      HACK = { icon = " ", color = "warning", alt = { "hack" } },
-      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX", "warn", "warning" } },
-      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "optim", "performance", "optimize" } },
-      NOTE = { icon = " ", color = "hint", alt = { "INFO", "note", "info" } },
+      TODO = { icon = " ", color = "info", alt = { "todo", "Todo" } },
+      HACK = { icon = " ", color = "warning", alt = { "hack", "Hack" } },
+      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX", "warn", "warning", "Warn", "Warning" } },
+      PERF = {
+        icon = " ",
+        alt = {
+          "OPTIM",
+          "PERFORMANCE",
+          "OPTIMIZE",
+          "optim",
+          "performance",
+          "optimize",
+          "Optim",
+          "Performance",
+          "Optimize",
+        },
+      },
+      NOTE = { icon = " ", color = "hint", alt = { "INFO", "note", "info", "Info", "Note" } },
       TEST = {
         icon = "⏲ ",
         color = "test",
-        alt = { "TESTING", "PASSED", "FAILED", "test", "testing", "passed", "failed" },
+        alt = {
+          "TESTING",
+          "PASSED",
+          "FAILED",
+          "test",
+          "testing",
+          "passed",
+          "failed",
+          "Test",
+          "Testing",
+          "Passed",
+          "Failed",
+        },
       },
     },
     search = {
-      pattern = [[\b(KEYWORDS)(:|!)]], -- ripgrep regex also check for rust macros
+      pattern = [[\b(KEYWORDS)\s*(:?|!)]], -- ripgrep regex also check for rust macros
     },
   },
 }
