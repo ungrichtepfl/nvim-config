2f922b252ab1ab0f4d6ad92e50d0735799278f37
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-02-02 14:33:46 +0100

    feat: Increase notification timeout.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,13 +3,15 @@ return {
   opts = {
     level = vim.log.levels.DEBUG,
     stages = "fade",
-    timeout = 2000,
+    timeout = 5000,
     max_width = function() return math.floor(vim.api.nvim_win_get_width(0) / 2) end,
     render = "wrapped-compact",
   },
   enabled = function() return vim.env.COLORTERM == "truecolor" or vim.env.TERM == "xterm-truecolor" end,
-  init = function()
+  config = function(_, opts)
+    local notify = require "notify"
+    notify.setup(opts)
     vim.o.termguicolors = true
-    vim.notify = require "notify"
+    vim.notify = notify
   end,
 }
