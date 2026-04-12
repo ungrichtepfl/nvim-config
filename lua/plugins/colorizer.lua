7314dd7f5b4794ec416cf038cc6cba706dd960e9
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-04-12 14:31:34 +0200

    fix: switch colorizer plugin
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,8 +1,5 @@
 return {
-  -- Show RGB and hex colors
-  "norcalli/nvim-colorizer.lua",
-  event = "VeryLazy",
-
-  -- NOTE: Does not work with empty opts dict so config is used:
-  config = function() require("colorizer").setup() end,
+  "catgoose/nvim-colorizer.lua",
+  event = "BufReadPre",
+  opts = {},
 }
